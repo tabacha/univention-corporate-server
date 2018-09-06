ed6c17534d0ab227ca2b6339a14de3dfc11cd859
Erik Damrose <damrose@univention.de>
2018-09-06 12:52:36 +0200

    Bug #47757: adapt run.sh

--- test/branch-tests/base/run.sh
+++ test/branch-tests/base/run.sh
@@ -7,7 +7,7 @@ cd test
 
 # update packages
 #./utils/build-branch-packages -g $GIT_BRANCH
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no jenkins@10.200.18.180 python3 build -r "https://gitlab+deplay-token-2:FAd2WHRZQqYFzLMZd5Q1@git.knut.univention.de/univention/ucs.git" -b "$GIT_BRANCH" > utils/apt-get-branch-repo.list
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@10.200.18.180 sudo -u jenkins python3 /home/jenkins/build -r "https://gitlab+deplay-token-2:FAd2WHRZQqYFzLMZd5Q1@git.knut.univention.de/univention/ucs.git" -b "$GIT_BRANCH" > utils/apt-get-branch-repo.list
 # echo "# ignore" > utils/apt-get-branch-repo.list
 
 declare -a cmd=("./ucs-ec2-tools/ucs-kvm-create" "-c" "branch-tests/base/$CFG_FILE")
