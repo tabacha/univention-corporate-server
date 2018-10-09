529fd9fb3da1c3a9de150abd3de7f4e6e87d3e41
Daniel Troeder <troeder@univention.de>
2018-10-09 07:45:55 +0200

    property encoding works now

--- management/univention-directory-manager-modules/modules/univention/udm/http_api/users_user.py
+++ management/univention-directory-manager-modules/modules/univention/udm/http_api/users_user.py
@@ -7,40 +7,57 @@ from ..udm import Udm
 from ..encoders import _classify_name
 
 try:
-	from typing import Dict, Optional, Text
+	from typing import Dict, Optional, Text, Union
 	import univention.admin.uldap
 	from ..base import BaseUdmModule
-	from ..binary_props import Base64BinaryProperty
 	from ..encoders import BaseEncoder
+	from ..binary_props import Base64BinaryProperty
 	from flask_restplus import Api
 except ImportError:
 	pass
 
 
+UDM_API_VERSION = 1
+
+
 class NoneList(fields.List):
 	def format(self, value):
-		logger.debug('value=%r', value)
-		return value if value else None
+		return None if value in ('', None, []) else value
+
+	def output(self, key, data, ordered=False, **kwargs):
+		# handle empty lists encoded as an empty strings which lead to
+		# AttributeErrors, when flask_restplus tries to serialize them
+		# happens for example with sambaLogonHours
+		if hasattr(data, key) and getattr(data, key) == '':
+			logger.debug('** getattr(data, %r)=%r', key, getattr(data, key))
+			setattr(data, key, [])
+		super(NoneList, self).output(key, data, ordered, **kwargs)
 
 
 class NoneString(fields.String):
 	def format(self, value):
-		logger.debug('value=%r', value)
-		return value if value not in ('', None) else None
+		return None if value in ('', None) else value
 
 
-class Base64BinaryProperty2String(fields.Raw):
-	def format(self, value):  # type: (Base64BinaryProperty) -> Text
-		assert isinstance(value, Base64BinaryProperty)
+class Base64BinaryProperty2StringOrNone(fields.Raw):
+	def format(self, value):  # type: (Union[Base64BinaryProperty, Text]) -> Union[Text, None]
+		if value == '':
+			return None
 		return value.encoded
+
+
+class NoneDateField(fields.Date):
+	def format(self, value):
+		res = None if value in ('', None) else value
+		return super(NoneDateField, self).format(res)
 
 
 type2field = {
 	bool: fields.Boolean,
-	datetime.date: fields.Date,
+	datetime.date: NoneDateField,
 	int: fields.Integer,
 	str: NoneString,
-	'Base64Binary': Base64BinaryProperty2String,
+	'Base64Binary': Base64BinaryProperty2StringOrNone,
 }
 
 
@@ -50,9 +67,9 @@ logger = logging.getLogger(__name__)
 def get_module(module_name, lo=None):  # type: (Text, Optional[univention.admin.uldap.access]) -> BaseUdmModule
 	logger.debug('get_module(module_name={!r}, lo={!r})'.format(module_name, lo))
 	if lo:
-		udm = Udm(lo, 1)
+		udm = Udm(lo, UDM_API_VERSION)
 	else:
-		udm = Udm.using_admin(1)
+		udm = Udm.using_admin(UDM_API_VERSION)
 	return udm.get(module_name)
 
 
@@ -61,7 +78,6 @@ def get_model(module_name, api, lo=None):
 	logger.debug('get_model(module_name={!r}, api={!r} lo={!r})'.format(module_name, api, lo))
 	mod = get_module(module_name=module_name, lo=lo)
 	obj = mod.new()
-	logger.debug('get_model({!r}) obj={!r}'.format(lo, obj))
 	identifying_udm_property = mod.meta.identifying_property
 	identifying_ldap_attribute = mod.meta.mapping.udm2ldap[identifying_udm_property]
 	res = OrderedDict((
@@ -77,19 +93,12 @@ def get_model(module_name, api, lo=None):
 		for prop, is_multivalue in props_is_multivalue.items()
 	)  # type: Dict[Text, fields.Raw]
 	encoders = mod._udm_object_class.udm_prop_class._encoders  # type: Dict[Text, BaseEncoder]
-	# logger.debug('** props={!r}'.format(props))
-	# logger.debug('** encoders={!r}'.format(encoders))
 	for prop in props.keys():
-		logger.debug('prop=%r', prop)
 		try:
 			encoder = encoders[prop]
-			logger.debug('encoder=%r', encoder)
-			# logger.debug('** prop={!r} encoder={!r}'.format(prop, encoder))
 			if hasattr(encoder.type, '__iter__'):
-				logger.debug('hasiter')
 				# nested object or list
 				prop_type, content_desc = encoder.type
-				logger.debug('prop_type=%r content_desc=%r', prop_type, content_desc)
 				if prop_type is dict:
 					field_type = fields.Nested
 					field_kwargs = {'skip_none': True}
@@ -105,7 +114,6 @@ def get_model(module_name, api, lo=None):
 					if isinstance(content_desc, dict):
 						try:
 							nested_structure = dict((k, type2field[v]) for k, v in content_desc.items())
-							logger.debug('nested_structure=%r', nested_structure)
 						except KeyError:
 							raise ValueError('Unknown encoder type in nested encoder: {!r}'.format(encoder.type))
 						else:
@@ -113,23 +121,16 @@ def get_model(module_name, api, lo=None):
 								api.model('{}Prop_{}'.format(_classify_name(mod.name), prop), nested_structure),
 								skip_none=True
 							)
-							logger.debug('field_content=%r', field_content)
 					else:
 						field_content = type2field[content_desc]
 				else:
 					raise ValueError('Unknown encoder type: {!r}'.format(encoder.type))
-				logger.debug('props[{!r}] = {!r}({!r}, **{!r})'.format(prop, field_type, field_content, field_kwargs))
 				props[prop] = field_type(field_content, **field_kwargs)
 			else:
-				logger.debug('encoder.type=%r', encoder.type)
 				props[prop] = type2field[encoder.type]
-				logger.debug('props[prop]=%r', props[prop])
-			# logger.debug('** props[{!r}]={!r}'.format(prop, props[prop]))
 		except KeyError:
 			pass
 	props = OrderedDict((k, props[k]) for k in sorted(props.keys()))
-	logger.debug('props=%r', props)
 	res['props'] = fields.Nested(api.model('{}Props'.format(_classify_name(mod.name)), props), skip_none=True)
 	res['uri'] = fields.Url('{}_{}'.format(mod.name, '_'.join(mod.name.split('/'))))
-	logger.debug('res={!r}'.format(res))
 	return res
