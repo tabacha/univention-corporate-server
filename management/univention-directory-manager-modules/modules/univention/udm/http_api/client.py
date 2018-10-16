ac7418c26e9d9f7ba8fac1b8e02bad4f851fca23
Daniel Troeder <troeder@univention.de>
2018-10-16 05:21:53 +0200

    add autogenerated python client

---
+++ management/univention-directory-manager-modules/modules/univention/udm/http_api/client.py
@@ -0,0 +1,60 @@
+import random
+import string
+from bravado.client import SwaggerClient
+
+
+def random_str():
+	return ''.join(random.choice(string.ascii_letters) for _ in range(8))
+
+
+client = SwaggerClient.from_url('http://10.20.30.5/udm/swagger.json')
+
+# LIST
+users = client.users_user.list().result()
+print('Got {} users.'.format(len(users)))
+
+# GET
+username = users[-1].props.username
+user = client.users_user.get(id=username).result()
+print('Unixhome of user {!r} is {!r}.'.format(user.props.username, user.props.unixhome))
+
+# CREATE
+hpa = [
+	{'street': random_str(), 'zipcode': random_str(), 'city': random_str()},
+	{'street': random_str(), 'zipcode': random_str(), 'city': random_str()},
+]
+udm_attrs = {
+	'firstname': random_str(),
+	'lastname': random_str(),
+	'username': random_str(),
+	'password': random_str(),
+	'homePostalAddress': hpa,
+}
+user = client.users_user.create(payload={
+	'id': udm_attrs['username'],
+	'props': udm_attrs,
+}).result()
+print('New user: {!r}\n'.format(user))
+user = client.users_user.get(id=udm_attrs['username']).result()
+for k, v in udm_attrs.items():
+	if k == 'password':
+		continue
+	print('Checking {!r}...'.format(k))
+	if isinstance(v, list):
+		# homePostalAddress
+		res = [_hpa.__dict__['_Model__dict'] for _hpa in getattr(user.props, k)]
+		assert sorted(res) == sorted(v), 'Expected for {!r}: {!r} Got: {!r}'.format(k, v, res)
+	else:
+		res = getattr(user.props, k)
+		assert res == v, 'Expected for {!r}: {!r} Got: {!r}'.format(k, v, res)
+	print('OK: {!r}'.format(k))
+
+# MODIFY
+print('Old unixhome of user {!r} is {!r}.'.format(user.props.username, user.props.unixhome))
+modification = {'props': {'unixhome': '{}2'.format(user.props.unixhome)}}
+user = client.users_user.modify(id=user.props.username, payload=modification).result()
+print('New unixhome of user {!r} is {!r}.'.format(user.props.username, user.props.unixhome))
+
+# DELETE
+result = client.users_user.delete(id=user.props.username).result()
+print('Result of deleting user {!r} is {!r} (no exception was raised).'.format(user.props.username, result))
